97583a98e82a5b24b0a1df923e8146c893fb77cf
Wambuingare <wambuinancyngare@gmail.com>
2022-11-01 07:30:38 +0300

    Solace-website

--- src/About.js
+++ src/About.js
@@ -1,27 +1,21 @@
 import React from "react";
 import './About.css';
 // import pics4 from "./cover.png";
-// import pics5 from "./images/Group 2.png";
 import pics6 from "./images/meditation.png";
 
 
 function About() {
     return (
-      <div className="About-us">
+      <div className="About-us" id="about">
       
         <div className="head">
            <h1>About Solace</h1>
         </div>
 
-      <div className="words">
-      {/* <img src={pics4} alt="cover" width='100%'/>  */}
-        <h3>Solace is a mobile application that aims to assist Gen Z in 
-Kenya manage anxiety disorder.It provides interactive self
--help and resources such as guided meditation to help 
-users to manage symptoms associated with anxiety 
-disorder.It allows them to chat and share how they are 
-feeling everyday in order to improve their quality of life.</h3>
-
+      <div className="info">
+      {/* <img src={pics4} alt="cover" width='100%' height='150%'/>  */}
+        <h3>Anxiety disorders have been identified as the most common mental illness in Kenya. Solace seeks to protect Kenya's Generation Z population from various types of anxiety disorders and to help cut off symptoms associated with anxiety by developing a smartphone application.
+The mobile application's main goal is to help Gen Z in Kenya manage their anxiety disorder and improve their quality of life by providing tools such as interactive self-help, guided meditation, breathing exercises, and other evidence-based techniques.</h3>
 </div>
 <div className="steps">
 <h1 id="h1">Features of the App</h1>
@@ -34,9 +28,7 @@ feeling everyday in order to improve their quality of life.</h3>
    
 
 
-<div className="Med"> 
-{/* 
-<img id="pics5" src={pics5} alt="phone"/>  */}
+<div className="Meditation"> 
 <img id="med" src={pics6} alt="meditate" width='40%'/> 
 </div>
 
--- src/Home.js
+++ src/Home.js
@@ -1,27 +1,22 @@
 import React from "react";
 import './App.css';
 import './Home.css';
-// import pics from "./images/Group 1.png";
-// import pics2 from "./images/Group 2.png";
-// import pics3 from "./images/Group 3.png";
 import pic from "./images/sol.png";
 
 function Home() {
     return (
       <div className="App">
-        {/* <h1>Home page</h1> */}
-
-          <div className="info">
-        <h2>Solace</h2>git credential-manager uninstall
+             <div className="Title">
+        <h2>Solace</h2>
         <h1>Find Your Peace</h1>
-        <h3>Our goal is to help you manage<br></br>
+        <h3>Our main goal is to help you manage
             anxiety disorder.</h3>
             </div>
-  
-      <div className="Landing">
-         <img src={pic}alt='page' width='150%'/> 
+   
+   <div className="Landing">
+         <img src={pic}alt='page' width='50%'/> 
         </div>
-      </div>
+        </div>
     );
   } 
  

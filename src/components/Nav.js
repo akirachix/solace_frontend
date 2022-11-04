c4b72f40452dd13ec8c6868f8659f04a4e153e24
Wambuingare <wambuinancyngare@gmail.com>
2022-11-04 09:31:39 +0300

    solace

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,3 +1,4 @@
+
 import React, { useState } from 'react';
 import '../styles/Nav.css';
 import pic from "../images/SolaceLogo.png";
@@ -32,10 +33,21 @@ return (
       <li ><a href='#about' >About</a></li>
       <li> <a href='#team' >Team</a></li>
     </ul>
-   
+    {/* <div className='hamburger'>
+     {MenuOpen ? <MenuOpenIcon onClick={open_}/>:<MenuIcon onClick={close_}/>}
+ 
+   </div> */}
+ 
+    <div className='mobileNav' style={styles}>
+    
+   <ul className='mobile-menu'>
+   <li ><a href='#home' >Home</a></li>
+     <li ><a href='#about' >About</a></li>
+     <li> <a href='#team' >Team</a></li>
+   </ul>
+   </div>
     </div>
 );
 }
 
 export default Nav;
-
